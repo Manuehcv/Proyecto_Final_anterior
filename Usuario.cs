3ef70bb8817dd386b19f6c8633d4316861692572
Manueh1715 <manuelcv1715@outlook.com>
2023-01-31 16:58:41 -0500

    Agregar archivos de proyecto.

---
+++ Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_final
+{
+    public class Usuario
+    {
+        private int id;
+        private string nombre;
+        private string apellido;
+        private string nombreUsuario;
+        private string contraseña;
+        private string mail;
+    }
+}
