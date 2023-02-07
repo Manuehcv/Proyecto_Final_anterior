d02cb14ea52ec7368a4f1888c4b1d8a4a225d8eb
Manueh1715 <manuelcv1715@outlook.com>
2023-02-06 20:35:59 -0500

    Agregué el método login para el manejadorUsuario y el test unitario.

--- ManejadorUsuario.cs
+++ ManejadorUsuario.cs
@@ -40,6 +40,37 @@ namespace Manuel_Carvajal
             }
             return user;
         }
+        public static Usuario Login(string nombreUsuario, string contraseña)
+        {
+            Usuario usuario = new Usuario();
+            const string cadenaConexion = "Data Source=DESKTOP-JR4NFDN;Initial Catalog=SistemaGestion;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            var query = "SELECT * FROM Usuario WHERE NombreUsuario = @nombreUsuario AND Contraseña = @contraseña";
+            using (SqlConnection conn = new SqlConnection(cadenaConexion))
+            {
+                SqlCommand comando = new SqlCommand(query, conn);
+                comando.Parameters.AddWithValue("@nombreUsuario", nombreUsuario);
+                comando.Parameters.AddWithValue("@contraseña",contraseña);
+                conn.Open();
+                using (SqlDataReader reader = comando.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        reader.Read();
+                        usuario.Id = Convert.ToInt64(reader.GetInt64(0));
+                        usuario.Nombre = reader.GetString(1);
+                        usuario.Apellido= reader.GetString(2);
+                        usuario.NombreUsuario= reader.GetString(3);
+                        usuario.Contraseña= reader.GetString(4);
+                        usuario.Mail= reader.GetString(5);
+
+                    }
+                conn.Close();
+                }    
+
+
+            }
+         return usuario;
+        }
 
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,7 +9,8 @@ namespace Manuel_Carvajal
         {
             //testGetUsuario(2);
             //testGetProductsByUserID(1);
-            testGetVentaByUserID(1);
+            //testGetVentaByUserID(1);
+            testUserLogIn("eperez", "SoyErnestoPerez");
 
         }
 
@@ -54,6 +55,29 @@ namespace Manuel_Carvajal
                 Console.WriteLine(product.IdUsuario.ToString());
             }
         }
+        static void testUserLogIn(string user, string password)
+        {
+            Usuario testUser = ManejadorUsuario.Login(user, password);
+            if (testUser.Id == 0)
+            {
+
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine("El Usuario o Contraseña ingresados no se corresponden con la base de datos.");
+                Console.WriteLine("---------------------------------");
+                return;
+            }
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("Usuario Logeado Exitosamente");
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine(testUser.Id.ToString());
+            Console.WriteLine(testUser.Nombre);
+            Console.WriteLine(testUser.Apellido);
+            Console.WriteLine(testUser.NombreUsuario);
+            Console.WriteLine(testUser.Contraseña);
+            Console.WriteLine(testUser.Mail);
+
+        }
+
     }
 
 }
